02edae348224a65d76a4f3f8c7491e01ad98b08f
Emil Vaklinov <emil_vaklinov@yahoo.com>
2019-04-14 11:53:26 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-/*
-window.onload = function() {
-
-    var images = [
-        image1.jpg,
-        image2.jpg,
-        image3.jpg,
-        ];
-
-    var prev = document.getElementById("prev");
-    var next = document.getElementById("next");
-    var img = document.getElementsByTagName("img");
-    var i = 0, r;
-
-    prev.addEventListener("click", function() {move(-1)});
-    next.addEventListener("click", function() {move(+1)});
-
-    function move(r) {
-        i += r;
-        i >= images.length ? i = 0 : i;
-        i < 0 ? i = images.length -1 : i;
-        img[0].src = images[i];
-
-    }
-    */
 
 window.onscroll = function() {scrollFunction()};
 
@@ -39,3 +14,4 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
